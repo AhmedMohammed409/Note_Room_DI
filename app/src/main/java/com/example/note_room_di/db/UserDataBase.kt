309a65a5ecacec0f4978268e700270404ac3ddd6
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-12-15 22:28:28 +0200

    Fix bugs

--- app/src/main/java/com/example/note_room_di/db/UserDataBase.kt
+++ app/src/main/java/com/example/note_room_di/db/UserDataBase.kt
@@ -7,16 +7,5 @@ import com.example.note_room_di.models.Note
 
 @Database(entities = [Note::class], version = 1, exportSchema = false)
 abstract class  UserDataBase:RoomDatabase() {
-
     abstract fun userDao(): UserDAO
-    /*companion object{
-        private var dbInstance:UserDataBase?=null
-        fun getDataBaseInstance(context: Context): UserDataBase {
-            if (dbInstance==null){
-            return Room.databaseBuilder(
-                context, UserDataBase::class.java, "Note_database").fallbackToDestructiveMigration().build()
-        }
-            return dbInstance!!
-        }
-    }*/
 }
